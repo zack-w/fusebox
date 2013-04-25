e8c612af8bac8094734aa011537011a8227abab8
Zack <zack@exolocity.com>
2013-04-24 23:58:28 -0400

    Tuched up Login Page
    - Email carry's over onto form
    - Added a better error message
    - Finished removing messages usage
    - Fixed issue with user controller not loading view

--- www2/fusebox/views/login.php
+++ www2/fusebox/views/login.php
@@ -6,16 +6,16 @@
 			<h1><?php echo lang("login_heading"); ?></h1>		
 			<div class="login-fields">
 				
-				<p><?php echo lang("login_subheading"); ?></p>
+				<p><? echo lang("login_subheading"); ?></p>
 				
 				<div class="field">
-					<label for="username"><?php echo lang("index_email_th"); ?>:</label>
-					<input type="text" id="identity" name="identity" value="" placeholder="<?php echo lang("index_email_th"); ?>" class="login username-field" />
+					<label for="username"><? echo lang("index_email_th"); ?>:</label>
+					<input type="text" id="identity" name="identity" value="<? echo $this->data[ "email_fill" ]; ?>" placeholder="<? echo lang("index_email_th"); ?>" class="login username-field" />
 				</div>
 				
 				<div class="field">
-					<label for="password"><?php echo lang("login_password_label"); ?>:</label>
-					<input type="password" id="password" name="password" value="" placeholder="<?php echo lang("login_password_label"); ?>" class="login password-field"/>
+					<label for="password"><? echo lang("login_password_label"); ?>:</label>
+					<input type="password" id="password" name="password" value="" placeholder="<? echo lang("login_password_label"); ?>" class="login password-field"/>
 				</div>
 				
 			</div>
@@ -23,10 +23,21 @@
 			<div class="login-actions">
 				<span class="login-checkbox">
 					<input id="Field" name="remember" type="checkbox" class="field login-checkbox" value="First Choice" tabindex="4" />
-					<label class="choice" for="Field"><?php echo lang("login_remember_label"); ?></label>
+					<label class="choice" for="Field"><? echo lang("login_remember_label"); ?></label>
 				</span>		
-				<button class="button btn btn-warning btn-large"><?php echo lang("login_submit_btn"); ?></button>
+				<button class="button btn btn-warning btn-large"><? echo lang("login_submit_btn"); ?></button>
 			</div>
 		</form>
 	</div>
+	
+	<?php
+		if( !empty( $AccountLoginError ) )
+		{
+			echo "
+				<div class='account-login-failed'>
+					Username or password is incorrect.
+				</div>
+			";
+		}
+	?>
 </div>
