cbe8bb0ed7597a836da0764c82033dba3d7dedf3
Patrick Hampson <patrickhampson@gmail.com>
2013-04-25 22:52:15 -0400

    Updates to update system

--- www2/fusebox/views/usercp.php
+++ www2/fusebox/views/usercp.php
@@ -24,14 +24,6 @@
 						<form id="edit-profile" class="form-horizontal" method="post" action="user/update/">
 							<fieldset>
 								<div class="control-group">											
-									<label class="control-label" for="username">Username</label>
-									<div class="controls">
-										<input type="text" class="input-medium disabled" id="username" value="<?php echo $user->username; ?>" disabled>
-										<p class="help-block">Your username is for logging in and cannot be changed.</p>
-									</div> <!-- /controls -->				
-								</div> <!-- /control-group -->
-							
-								<div class="control-group">											
 									<label class="control-label" for="firstname">First Name</label>
 
 									<div class="controls">
