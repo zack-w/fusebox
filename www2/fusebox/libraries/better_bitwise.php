d74dc97e8c82522d8a6a1561cd79ed92650c198d
Zack <zack@exolocity.com>
2013-06-29 00:18:26 -0400

    Fixed Bitwise

--- www2/fusebox/libraries/better_bitwise.php
+++ www2/fusebox/libraries/better_bitwise.php
@@ -18,17 +18,12 @@
 		
 		public function AddFlag( $CurBit, $Flag, $CharAt = 0 ) {
 			$NumBytes = strlen( $CurBit );
-			$Byte = substr( $Bitwise, $CharAt, 1 );
+			$Byte = substr( $CurBit, $CharAt, 1 );
 			
-			echo $CurBit;
-			echo "TotalLen: " . strlen( $CurBit ) . "<br />";
-			echo "Len: " . strlen( $Byte ) . "<br />";
-			echo "CharAt: " . $CharAt . "<br />";
-			
-			if( empty( $Byte ) ) {
-				$CurBit = $CurBit . chr( 0x00 );
-				$Byte = 0x00;
-			}else{
+			if( strlen( $Byte ) == 0 ) {
+				$Byte = 0;
+				$CurBit .= chr( 0x00 );
+			} else {
 				$Byte = ord( $Byte ) + 1;
 			}
 			
@@ -36,13 +31,9 @@
 				return AddFlag( $CurBit, $Flag - 8, $CharAt + 1 );
 			}
 			
-			echo $Byte . " : " . pow( 2, $Flag ) . "<br />";
-			
 			$Front = substr( $CurBit, 0, $CharAt );
 			$NewByte = chr( ($Byte | pow( 2, $Flag )) - 1 );
 			$End = substr( $CurBit, $CharAt + 1, strlen( $CurBit ) - ($CharAt + 1) );
-			
-			echo (ord( $NewByte ) + 1) . "<br />";
 			
 			return ($Front . $NewByte . $End);
 		}
